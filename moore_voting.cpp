bba00b1936691a13d67d3911488ec4218710f854
Rishi-Mishra0704 <rishimishra0404@gmail.com>
2025-01-16 02:07:01 +0530

    complete moore_voting

--- moore_voting.cpp
+++ moore_voting.cpp
@@ -1,8 +1,50 @@
 #include <iostream>
 
 using namespace std;
+int moore_voting(int arr[], int n)
+{
+    int i, candidate = -1, votes = 0;
+    for (i = 0; i < n; i++)
+    {
+        if (votes == 0)
+        {
+            candidate = arr[i];
+            votes = 1;
+        }
+        else
+        {
+            if (arr[i] == candidate)
+            {
+                votes++;
+            }
+            else
+            {
+                votes--;
+            }
+        }
+    }
 
+    int count = 0;
+    for (i = 0; i < n; i++)
+    {
+        if (arr[i] == candidate)
+        {
+            count++;
+        }
+    }
+    if (count > n / 2)
+    {
+        return candidate;
+    }
+    return -1;
+}
 
-int main(){
+int main()
+{
+      int arr[] = { 1, 1, 1, 1, 2, 3, 4 };
+    int n = sizeof(arr) / sizeof(arr[0]);
+    int majority = moore_voting(arr, n);
+    cout << " The majority element is : " << majority;
+    return 0;
     return 0;
 }
